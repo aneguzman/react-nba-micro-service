8147c4b54a8f8d604d68c4d577cba8dfba1767f3
Aneudy Guzman <aneguzman@gmail.com>
2017-11-09 21:05:20 -0400

    =Adding .env file and JS docs.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const request = require('request');
 const scrape = require('scrape-it');
+require('dotenv').config();
 
 const app = express();
-const port = 9000;
-const open = require('open');
+const port = process.env.PORT;
 
 const FEED_API_URL = 'https://www.thescore.com/nba/news?page=';
 const STANDINGS_API_URL = 'http://data.nba.net/10s/prod/v1/current/standings_conference.json';
@@ -12,23 +12,36 @@ const STANDINGS_API_URL = 'http://data.nba.net/10s/prod/v1/current/standings_con
 const getScoresApiUrl = date => `http://data.nba.net/prod/v1/${date}/scoreboard.json`;
 const getFeedsApiUrl = page => `${FEED_API_URL}${page}`;
 
+/**
+ * Request the list of NBA scores for a given date.
+ * @param {string} date - the date of the scores.
+ * @return {Object} the JSON object with the data.
+ */
 app.get('/api/scores/', (req, res) => {
-  const currentDate = req.param('date');
-  request(getScoresApiUrl(currentDate), (error, response, body) => {
+  const { date } = req.query;
+  request(getScoresApiUrl(date), (error, response, body) => {
     res.json(body);
   });
 });
 
+/**
+ * Request the list of NBA standings.
+ * @return {Object} the JSON object with the data.
+ */
 app.get('/api/standings/', (req, res) => {
   request(STANDINGS_API_URL, (error, response, body) => {
     res.json(body);
   });
 });
 
+/**
+ * Request a list of NBA articles.
+ * @param {number} page - the page number.
+ * @return {Object} the JSON object with the data.
+ */
 app.get('/api/feed', (req, res) => {
-  const page = req.query.page;
+  const { page } = req.query;
   scrape(getFeedsApiUrl(page), {
-    // Fetch the articles
     articles: {
       listItem: '.article-tile-wrapper',
       data: {
@@ -47,7 +60,5 @@ app.listen(port, (err) => {
   if (err) {
     const error = new Error(err);
     throw error;
-  } else {
-    open(`http://localhost:${port}`);
   }
 });
